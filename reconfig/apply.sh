6cb7e2aa4abe3d0e6f7bec1050529dcc013845c1
Heiner Bunjes <heiner.bunjes@btc-ag.com>
2019-01-17 13:40:46 +0100

    min

--- reconfig/apply.sh
+++ reconfig/apply.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+OUTPUT_FOLDER=../../docker-elk
 FILES_AND_FOLDERS_TO_BE_DELETED="docker-stack.yml .travis.yml LICENSE extensions/apm-server"
+FILES_TO_BE_COPIED=$(find ../.)
+
 
 echo "#### apply.sh has started."
-
-FILES_TO_BE_COPIED=$(find ../.)
-OUTPUT_FOLDER=../../docker-elk
 
 
 if ! [ -f $OUTPUT_FOLDER/README.md ]; then
@@ -16,15 +16,10 @@ else
 fi
 
 
-
-
 for inputfile in $FILES_AND_FOLDERS_TO_BE_DELETED
 do
-  
   outputfile=$OUTPUT_FOLDER/$inputfile
-  echo "Potentially deleting: '$outputfile' ..."
 
-  # take action on each file. $inputfile stores current file name
   if [ -f "$outputfile" ]; then
     echo "  deleting the file '$outputfile' ..."
     rm $outputfile
@@ -37,16 +32,10 @@ do
 done
 
 
-
-
 for inputfile in $FILES_TO_BE_COPIED
-do
-  #echo "Potentially copying: '$inputfile' ..."
-  
+do  
   outputfile=$OUTPUT_FOLDER/${inputfile#.././}
-  #echo "outputfile = '$outputfile'"
 
-  # take action on each file. $inputfile stores current file name
   if ! [ -f "$inputfile" ]; then
     : #echo "  $inputfile was no regular file. So it was not processed."
   elif [[ $inputfile =~ reconfig ]]; then
