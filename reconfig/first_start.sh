11ab347f5f22b6bd6aa5c5df42ff4a6e826fafc0
Heiner Bunjes <heiner@bunjes.net>
2019-01-18 22:15:43 +0100

    min

--- reconfig/first_start.sh
+++ reconfig/first_start.sh
@@ -6,8 +6,6 @@ DOCKER_ELK_RECONFIG_BRANCH=develop
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
 sudo sysctl -w vm.max_map_count=262144
 
-clear
-
 # Checkout docker-elk-reconfig and cache git login
 cd ~
 rm -rf ~/docker-elk-reconfig
