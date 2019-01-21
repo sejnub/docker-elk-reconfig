9cd324196507a2e7c5eca58f870459f148d63757
Heiner Bunjes <heiner.bunjes@btc-ag.com>
2019-01-21 16:54:22 +0100

    removed --build from compose up

--- reconfig/start.sh
+++ reconfig/start.sh
@@ -4,12 +4,13 @@ echo "#### start.sh has started."
 
 cd ~/docker-elk
 
+# add --build to check for new Dockerfiles
 docker-compose \
   -f docker-compose.yml \
   -f extensions/curator/curator-compose.yml \
   -f extensions/cerebro/cerebro-compose.yml \
   -f extensions/logspout/logspout-compose.yml \
-  up -d --build
+  up -d
 
 docker-compose \
   -f extensions/portainer/portainer-compose.yml \
