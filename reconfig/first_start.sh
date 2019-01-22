b63b1ce17083c14485f7f943fa42fd10dc6331dc
Heiner Bunjes <heiner.bunjes@btc-ag.com>
2019-01-22 13:39:36 +0100

    added branch parameter to first_start.sh

--- reconfig/first_start.sh
+++ reconfig/first_start.sh
@@ -3,6 +3,14 @@
 #TODO: Take branch as parameter here!
 DOCKER_ELK_RECONFIG_BRANCH=develop
 
+echo "#### first_start.sh has started."
+
+if [ $# -gt 0 ]; then
+  echo "first_start was called without a branch parameter. Taking taking the default: '$DOCKER_ELK_RECONFIG_BRANCH'"
+else
+  DOCKER_ELK_RECONFIG_BRANCH=$1  
+  echo "Cloning the branch '$DOCKER_ELK_RECONFIG_BRANCH' of docker-elk-reconfig."
+fi
 
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
 sudo sysctl -w vm.max_map_count=262144
@@ -18,3 +26,5 @@ git checkout $DOCKER_ELK_RECONFIG_BRANCH
 cd ~
 cd ~/docker-elk-reconfig/reconfig
 ./stop-clone-apply-start-wait-init.sh
+
+echo "#### first_start.sh has ended."
